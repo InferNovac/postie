a1de3fb6ef3b330d793e7aff194d587ed42cd74b
InferNovac <45517444+InferNovac@users.noreply.github.com>
2020-12-22 00:04:01 +0000

    Updated Sign Up more functional layout

--- src/components/structured_pages/sign_up/SignUp.js
+++ src/components/structured_pages/sign_up/SignUp.js
@@ -1,105 +1,152 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { Input, Select, Button } from "../../profile_field/UserInterface";
 import "./signup.css";
 
-const FormFields = () => {
-    const [fields, setFields] = useState({
-        name: "",
-        email: "",
-        gender: "",
-        userName: "",
-        userType: "",
-        password: "",
-        rePassword: "",
-    });
+const handleChange = (event, callback) => callback(event.target.value);
 
-    const handleChannge = (event) =>
-        setFields({ ...fields, [event.target]: event.target.value });
-
+const UserName = () => {
+    const [userName, setUserName] = useState("");
     return (
-        <React.Fragment>
-            <div className="row">
-                <div className="col">
-                    <Input
-                        type="text"
-                        name="userName"
-                        title="Username"
-                        value={fields.userName}
-                        handleChange={handleChannge}
-                        placeholder="Enter Profile Name"
-                    />
-                </div>
-                <div className="col">
-                    <Input
-                        title="Name"
-                        name="name"
-                        placeholder="Enter Name"
-                        type="text"
-                    />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <Input
-                        title="Email"
-                        name="email"
-                        placeholder="Enter E-mail"
-                        type="text"
-                    />
-                </div>
-                <div className="col">
-                    <Select
-                        title="Gender"
-                        name="gender"
-                        placeholder="Select Gender"
-                        options={["Male", "Female"]}
-                    />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <Select
-                        title="User Type"
-                        name="userType"
-                        placeholder="Select User Type"
-                        options={["Student", "Investor"]}
-                    />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <Input
-                        title="Password"
-                        name="password"
-                        placeholder="Enter password"
-                        type="password"
-                    />
-                </div>
-                <div className="col">
-                    <Input
-                        title="Re-enter Password"
-                        name="rePassword"
-                        placeholder="Re-enter Password"
-                        type="password"
-                    />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <Button disabled={true} type="sumbit" title="Sumbit">
-                        Sumbit
-                    </Button>
-                </div>
-            </div>
-        </React.Fragment>
+        <div className="col">
+            <Input
+                type="text"
+                name="userName"
+                title="Username"
+                value={userName}
+                handleChange={(event) => handleChange(event, setUserName)}
+                placeholder="Enter Profile Name"
+            />
+        </div>
     );
 };
 
+const Name = () => {
+    const [name, setName] = useState("");
+    return (
+        <div className="col">
+            <Input
+                title="Name"
+                name="name"
+                type="text"
+                value={name}
+                placeholder="Enter Name"
+                handleChange={(event) => handleChange(event, setName)}
+            />
+        </div>
+    );
+};
+
+const Email = () => {
+    const [email, setEmail] = useState("");
+
+    return (
+        <div className="col">
+            <Input
+                type="text"
+                name="email"
+                title="Email"
+                value={email}
+                placeholder="Enter E-mail"
+                handleChange={(event) => handleChange(event, setEmail)}
+            />
+        </div>
+    );
+};
+
+const Gender = () => {
+    const [gender, setGender] = useState("");
+    return (
+        <div className="col">
+            <Select
+                name="gender"
+                value={gender}
+                title="Gender"
+                placeholder="Select Gender"
+                options={["Male", "Female"]}
+                handleChange={(event) => handleChange(event, setGender)}
+            />
+        </div>
+    );
+};
+
+const UserType = () => {
+    const [userType, setUserType] = useState("");
+    return (
+        <div className="col">
+            <Select
+                name="userType"
+                value={userType}
+                title="User Type"
+                placeholder="Select User Type"
+                options={["Student", "Investor"]}
+                handleChange={(event) => handleChange(event, setUserType)}
+            />
+        </div>
+    );
+};
+
+const Password = () => {
+    const [password, setPassword] = useState("");
+    return (
+        <div className="col">
+            <Input
+                value={password}
+                type="password"
+                name="password"
+                title="Password"
+                placeholder="Enter password"
+                handleChange={(event) => handleChange(event, setPassword)}
+            />
+        </div>
+    );
+};
+
+const RePassword = () => {
+    const [rePassword, setRePassword] = useState("");
+    return (
+        <div className="col">
+            <Input
+                type="password"
+                name="rePassword"
+                value={rePassword}
+                title="Re-enter Password"
+                handleChange={(event) => handleChange(event, setRePassword)}
+                placeholder="Re-enter Password"
+            />
+        </div>
+    );
+};
+
+const FormFields = () => (
+    <form className="sign-up">
+        <div className="row">
+            <UserName />
+            <Name />
+        </div>
+        <div className="row">
+            <Email />
+            <Gender />
+        </div>
+        <div className="row">
+            <UserType />
+        </div>
+        <div className="row">
+            <Password />
+            <RePassword />
+        </div>
+        <div className="row">
+            <div className="col">
+                <Button disabled={true} type="sumbit" title="Sumbit">
+                    Sumbit
+                </Button>
+            </div>
+        </div>
+    </form>
+);
+
 const SignUp = () => (
     <div className="h-100 center-y">
-        <form className="sign-up">
-            <FormFields />
-        </form>
+        <FormFields />
     </div>
 );
 
